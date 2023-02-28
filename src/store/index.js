70c5dc48870698ee60b764a6780793f247cd2f3c
Svetlana Tomašik <seka.corazon@gmail.com>
2023-02-28 08:39:55 +0100

    dispatching actions from inside components

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore } from "redux";
 
 const counterReducer = (state = { counter: 0 }, action) => {
-  if (action.type === " increment") {
+  if (action.type === "increment") {
     return {
       counter: state.counter + 1,
     };
